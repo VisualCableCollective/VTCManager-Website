74667b3b6c6a1b21b294e8349594bb6bf2469156
Joschua H <joschua.hass.sh@gmail.com>
2022-05-24 14:14:43 +0200

    feature(sentry): added sentry support

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // dev code
+} else {
+    Sentry.init({
+        dsn: "https://27a4b342f56f4d6290a5303b9276235f@o1249248.ingest.sentry.io/6436432",
+        integrations: [new BrowserTracing()],
+
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    });
+}
 
 root.render(
     <React.StrictMode>
