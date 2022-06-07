49c10cd2414c71e8d660f57f760520645bf7515c
Joschua H <joschua.hass.sh@gmail.com>
2022-06-07 11:31:16 +0200

    imported theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '25vh': '25vh',
+      },
+      minHeight: {
+        '25vh' : '25vh',
+      },
+      maxWidth: {
+        '800px': '800px',
+      },
+      backgroundColor: {
+        'dark-1': '#0d0d0d',
+        'dark-2': '#121212',
+        'dark-3': '#1c1c1c',
+        'dark-4': '#2b2b2b',
+        'dark-5': '#363636',
+        'dark-6': '#3d3d3d',
+        'sidebar': '#1c1c1c',
+      },
+    },
+  },
+  variants: {
+    opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    extend: {
+      backgroundOpacity: ['active'],
+    }
   },
   plugins: [],
 }
