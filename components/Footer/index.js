d7a21db9f8f6f2c2f39653a50af1304ef7c6b5ef
Joschua H <54814804+joschiservice@users.noreply.github.com>
2022-10-21 15:24:53 +0200

    tweak(footer): design

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,10 +1,13 @@
 import AppConfig from "../../models/AppConfig";
 
+import {FaInstagram, FaTwitter} from "react-icons/fa";
+import {BsDiscord} from "react-icons/bs";
+
 export const Footer = () => {
     return (
-        <footer className="w-full bg-dark-3 text-white" style={{minHeight: '50vh'}}>
-            <div className="mx-auto xl:max-w-6xl pt-12">
-                <div className="flex flex-wrap justify-between px-6">
+        <footer className="w-full bg-dark-3 text-white pb-8">
+            <div className="mx-auto max-w-6xl pt-12">
+                <div className="flex flex-wrap justify-between px-8">
                     <div className="block">
                         <h1 className="text-xl font-bold pb-3">Information</h1>
                         <ul>
@@ -31,15 +34,13 @@ export const Footer = () => {
                     </div>
                 </div>
                 <hr className="border-dark-1 my-8"/>
-                <p className="text-center text-gray-300">Copyright ©2021-{new Date().getFullYear()} | Made With ❤️ and ☕ by <a
+                <p className="text-center text-gray-300 mx-6">Copyright ©2021-{new Date().getFullYear()} | Made With ❤️ and ☕ by <a
                     href="https://vcc-online.eu/" className="hover:text-white">The VisualCable Collective</a></p>
-                <div className="flex items-center justify-center pt-5">
+                <div className="flex items-center justify-center pt-5 pb-5 text-2xl">
                     <a href="https://vcc-online.eu/redirect/instagram"
-                       className="text-gray-300 hover:text-white px-2"><i className="fab fa-instagram fa-2x"/></a>
-                    <a href="https://vcc-online.eu/redirect/twitter" className="text-gray-300 hover:text-white px-2"><i
-                        className="fab fa-twitter fa-2x"/></a>
-                    <a href="https://vcc-online.eu/redirect/discord" className="text-gray-300 hover:text-white px-2"><i
-                        className="fab fa-discord fa-2x"/></a>
+                       className="text-gray-300 hover:text-white px-2"><FaInstagram /></a>
+                    <a href="https://vcc-online.eu/redirect/twitter" className="text-gray-300 hover:text-white px-2"><FaTwitter /></a>
+                    <a href="https://vcc-online.eu/redirect/discord" className="text-gray-300 hover:text-white px-2"><BsDiscord /></a>
                 </div>
             </div>
         </footer>
