a0d629e40db56d9bf15b3be04df5c874c3e80cbe
Joschua H <54814804+joschiservice@users.noreply.github.com>
2022-10-21 10:30:42 +0200

    fix(middleware): error when reloading auth pages

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,25 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import AppConfig from './models/AppConfig';
 
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   if (!request.cookies.get('vtcm_session'))
     return NextResponse.redirect(new URL('/login', request.url));
 
   let url = AppConfig.server_url + 'api/webapp/check';
 
-  const authResponse = await fetch(url, { headers: new Headers({ 'Authorization': 'Bearer ' + atob(request.cookies.get('vtcm_session')), 'Accept': 'application/json' }) });
-
-  if (!authResponse.ok) return NextResponse.redirect(new URL('/login', request.url));
-
-  const authResult = await authResponse.json();
-
-  if (!authResult["id"]) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+  fetch(url, { headers: new Headers({ 'Authorization': 'Bearer ' + atob(request.cookies.get('vtcm_session')), 'Accept': 'application/json' }) }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }).then(authResult => {
+    if (!authResult["id"]) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }).catch(e => {
+    console.log(e);
+  });
 }
 
 export const config = {
