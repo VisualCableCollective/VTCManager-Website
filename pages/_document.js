158aee23dd2c9bad7477c525362b0cb9c65e9846
Joschua H <joschua.hass.sh@gmail.com>
2022-10-26 10:09:29 +0200

    fix(adsbygoogle): page freeze

--- pages/_document.js
+++ pages/_document.js
@@ -14,10 +14,7 @@ export default function Document() {
                 crossOrigin="anonymous"
                 data-cookiecategory="targeting"
                 strategy="beforeInteractive"
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3288402026194745"
-                onError={(e) => {
-                    console.error('Google Adsense Script failed to load', e)
-                }} />
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3288402026194745"/>
             </body>
         </Html>
     )
