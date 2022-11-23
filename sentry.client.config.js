9ea7a7ad3999ec463a1f3574a20439857fa5a76e
Joschua H <joschua.hass.sh@gmail.com>
2022-11-23 10:30:03 +0100

    tweak(sentry): disabled in development

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
+console.log(process.env.NODE_ENV)
 Sentry.init({
   dsn: SENTRY_DSN || 'https://27a4b342f56f4d6290a5303b9276235f@o1249248.ingest.sentry.io/6436432',
   // Adjust this value in production, or use tracesSampler for greater control
@@ -14,5 +14,7 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  environment: process.env.NODE_ENV
+  environment: process.env.NODE_ENV,
+
+  enabled: process.env.NODE_ENV !== "development"
 });
